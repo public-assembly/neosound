a02726854d0fe24c7fda406b5a006583667c23eb
Dain Blodorn Kim <dain@db13.us>
2022-10-04 13:13:23 -0700

    sanitzing getListings return

--- components/Playlist/AudioPlayer.tsx
+++ components/Playlist/AudioPlayer.tsx
@@ -1,8 +1,16 @@
 import { EditionsAudioPlayer } from '@public-assembly/audio-player-ui'
 import { usePlaylistProvider } from '@/context/PlaylistProvider'
+import { useMemo } from 'react'
 
 export function AudioPlayer() {
-  const { playList } = usePlaylistProvider()
+  const { curationPlaylist } = usePlaylistProvider()
+
+  const playList = useMemo(() => {
+    const contracts = curationPlaylist.map((item: any) => item?.curatedContract)
+    console.log(contracts)
+    return contracts
+  }, [curationPlaylist])
+
   return (
     <EditionsAudioPlayer contractAddresses={playList as string[]} />
   )
--- context/PlaylistProvider.tsx
+++ context/PlaylistProvider.tsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from 'react'
 import { useContractRead } from 'wagmi'
 import Contract from './../abi/CurationManager.json'
 import { useCurationFunctions } from '@public-assembly/assemble-curation-functions'
-import { useEffect } from 'react'
+import { useMemo } from 'react'
 
 export type PlaylistProps = {
   children?: ReactNode
@@ -43,16 +43,44 @@ export function PlaylistProvider({ children, curationContractAddress, chainId }:
   })
 
   const { 
-    getListingsRead: playlistData, 
+    getListingsRead: playlistData,
     // getListingsError,
     // getListingsLoading,
   } = useCurationFunctions({
     curationContractAddress
   })
 
-  useEffect(() => {
-    console.log(chainId)
-    console.log(playlistData)
+  const santizedPlaylist = useMemo(() => {
+    const curationTargetTypes = {
+      '0': 'CURATION_TYPE_GENERIC',
+      '1': 'CURATION_TYPE_NFT_CONTRACT',
+      '2': 'CURATION_TYPE_CURATION_CONTRACT',
+      '3': 'CURATION_TYPE_CONTRACT',
+      '4': 'CURATION_TYPE_NFT_ITEM',
+      '5': 'CURATION_TYPE_EOA_WALLET',
+    }
+
+    function returnCurationType(key: keyof typeof curationTargetTypes) {
+      return curationTargetTypes[key]
+    }
+    if (playlistData) {
+      return playlistData.map((entry) => {
+        try {
+          return {
+            curatedContract: entry['curatedContract'],
+            curationTargetType: returnCurationType(entry['curationTargetType'].toString()),
+            hasTokenId: entry['hasTokenId'],
+            tokenId: entry['tokenId']?.toString(),
+            curator: entry['curator'],
+            sortOrder: entry['sortOrder'],
+          }
+        } catch (err) {
+          console.error(err)
+        }
+      })
+    } else {
+      return []
+    }
   }, [playlistData])
 
   return (
@@ -61,7 +89,7 @@ export function PlaylistProvider({ children, curationContractAddress, chainId }:
         toggleLayout,
         gridLayout,
         playList: data && data.length ? data : [],
-        curationPlaylist: playlistData && playlistData.length ? playlistData : [],
+        curationPlaylist: santizedPlaylist,
         chainId: chainId || '1',
       }}>
       {children}
