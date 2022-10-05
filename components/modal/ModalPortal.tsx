370540af8116a7fd9686cb465519f255f7f26322
Dain Blodorn Kim <dain@db13.us>
2022-10-04 19:26:55 -0700

    portal client side thing

--- components/modal/ModalPortal.tsx
+++ components/modal/ModalPortal.tsx
@@ -3,15 +3,16 @@ import { createPortal } from 'react-dom'
 import { isClientSide } from '@/utils/window'
 
 export const ModalPortal: React.FC<{children?: React.ReactNode}> = ({ children }) => {
-  if (!isClientSide) return null
-  
-  const el = React.useRef(document.createElement('div'))
+
+  const el = React.useRef(document?.createElement('div'))
 
   React.useEffect(() => {
     const popoutRoot = document.querySelector('#modal-root') as HTMLElement
     popoutRoot!.appendChild(el.current)
     return () => void popoutRoot!.removeChild(el.current)
   }, [])
+
+  if (!isClientSide) return null
 
   return createPortal(children, el.current)
 }
