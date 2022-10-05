57a5e80226e6bc06eedf8840787101c28300200f
Dain Blodorn Kim <dain@db13.us>
2022-10-05 14:07:02 -0700

    window width hook fix

--- hooks/useWindowWidth.ts
+++ hooks/useWindowWidth.ts
@@ -1,19 +1,23 @@
-/*
 import { useMemo, useEffect, useState } from 'react'
-import { breakpoints } from '@lib/breakpoints';
+
+export const breakpoints = {
+  sm: 768,
+  md: 1024,
+  lg: 1440,
+}
 
 export const useWindowWidth = () => {
-  const [windowWidth, setWindowWidth] = useState(undefined);
+  const [windowWidth, setWindowWidth] = useState(undefined)
 
   useEffect(() => {
     function handleResize() {
-      setWindowWidth(window.innerWidth);
+      setWindowWidth(window.innerWidth)
     }
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   const isLarge = useMemo(() => {
     return windowWidth && windowWidth >= breakpoints.md ? true : false
   }, [windowWidth])
@@ -26,7 +30,6 @@ export const useWindowWidth = () => {
     isLarge,
     isMedium,
     ww: windowWidth,
-    breakpoints
-  };
+    breakpoints,
+  }
 }
-*/
