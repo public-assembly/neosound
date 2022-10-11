5e6e9ceabb4c3069d679d853ea519b5bc14287d8
0xTranqui <thinkandchill11@gmail.com>
2022-10-11 10:50:50 -0500

    remove old deps + update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const About: NextPage = () => {
   return (
@@ -10,38 +9,17 @@ const About: NextPage = () => {
         <meta name="description" content="About Neosound" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col items-start justify-between gap-4 whitespace-pre-wrap px-6 text-stone-200 sm:px-12 md:max-w-4xl">
-        <div className="w-full sm:w-72">
-          <Image
-            src="/logos/logo.svg"
-            alt="logo"
-            width={900}
-            height={100}
-            objectFit="cover"
-            layout="responsive"
-          />
-        </div>
-        <h1 className="mt-12 text-3xl font-bold">Introduction</h1>
-        <p className="font-light text-stone-300">
-          In contrast to the physical world, only one degree of separation lies between
-          everyone on the internet. Today, we are exploring tools such as Neosound that
-          help our technology and expand our circles of trust while allowing for new
-          models of coordination + creation to take stage...but what exactly is Neosound?
-        </p>
-        <p className="font-light text-stone-300">
-          Neosound is a modular music player prototype designed to cater to hyper-local
-          communities, such as netlabels, with specific needs, tastes, and overall: aims
-          to promote identity in the digital realm. Not everyone wants to be the next big
-          thing in blockchain, or the next big platform so Neosound was created for that
-          reason.{' '}
-        </p>
-        <p className="font-light text-stone-300">
-          Specifically, this is a direct synthesis of microplatforms like unun, present
-          material, and door. It&apos;s an open-source build for anyone to use as a
-          curational tool to elevate music to their most open level. Neosound is not so
-          much focused on aggregation but instead on creating small, intimate, and
-          collaborative listening spaces for digital communities to grow.
-        </p>
+      <div className=" flex flex-col items-start justify-between gap-4 whitespace-pre-wrap px-6 text-stone-200 sm:px-12 md:max-w-4xl">
+        <h1 className="my-4 font-light text-stone-300 text-3xl">
+          Neosound
+        </h1>
+        <h1 className="text-xl sm:text-2xl font-light text-stone-300">
+        This site offers a new way to listen, share, and publish music on the internet. Built upon modular + open-source building blocks, Neosound will serve as a living reference for others to draw inspiration, learn, and remix for their own communities and audiences          {' '}
+        </h1>
+
+        <h1 className="text-xl sm:text-2xl font-light text-stone-300">
+        Combining the concepts of the microplatform, community-driven curation, and art focused consumption experiences, this project is a direct synthesis of Unun, Present Material, and Door. It is a template to help others create what’s missing. 
+        </h1>
       </div>
     </>
   )
