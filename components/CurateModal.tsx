4960f9374310c468f7ecc54345847e0909cabb62
Dain Blodorn Kim <dain@db13.us>
2022-10-11 13:42:13 -0700

    bump curation interface and add inline connect button

--- components/CurateModal.tsx
+++ components/CurateModal.tsx
@@ -13,19 +13,15 @@ export function CurateModal({ trigger }: { trigger?: JSX.Element }) {
       <Modal
         modalName="curation-interface"
         content={
-          <AuthCheck
-            formUI={
-              <CurationInterface
-                curationContractAddress={process.env.NEXT_PUBLIC_CURATION_CONTRACT}
-                network={Number(process.env.NEXT_PUBLIC_CHAIN_ID)}
-                closeButton={
-                  <button onClick={requestClose} className="justify-end">
-                    Close
-                  </button>
-                }
-                connectButton={<Connect />}
-              />
+          <CurationInterface
+            curationContractAddress={process.env.NEXT_PUBLIC_CURATION_CONTRACT}
+            network={Number(process.env.NEXT_PUBLIC_CHAIN_ID)}
+            closeButton={
+              <button onClick={requestClose} className="justify-end">
+                Close
+              </button>
             }
+            connectButton={<Connect />}
           />
         }
         trigger={trigger}
