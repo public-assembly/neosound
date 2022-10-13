528da3062f87c371b08e04fd1e1708489f122165
Dain Blodorn Kim <dain@db13.us>
2022-10-12 19:40:46 -0700

    curation targettypes

--- context/PlaylistProvider.tsx
+++ context/PlaylistProvider.tsx
@@ -9,6 +9,15 @@ export type PlaylistProps = {
   networkId?: '1' | '5'
 }
 
+export type CurationTargetTypes =
+  | 'CURATION_TYPE_GENERIC'
+  | 'CURATION_TYPE_NFT_CONTRACT'
+  | 'CURATION_TYPE_CONTRACT'
+  | 'CURATION_TYPE_CURATION_CONTRACT'
+  | 'CURATION_TYPE_NFT_ITEM'
+  | 'CURATION_TYPE_WALLET'
+  | 'CURATION_TYPE_ZORA_EDITION'
+
 export type PlayListReturn = {
   curatedAddress?: string
   curationTargetType?: CurationTargetTypes
